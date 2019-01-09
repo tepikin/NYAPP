7dc6af339c8724e474ab909bf00c077dfa0ae07e
tepikin <tepikinegor@gmail.com>
2019-01-09 12:27:59 +0700

    ValentinesDay app added

--- app/src/lite/java/com/lazard/nyapp/nyapp/ui/BaseActivityImpl.kt
+++ app/src/lite/java/com/lazard/nyapp/nyapp/ui/BaseActivityImpl.kt
@@ -2,22 +2,36 @@ package com.lazard.nyapp.nyapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import io.fabric.sdk.android.services.settings.IconRequest.build
+import android.provider.Settings.Secure
+import android.util.Log
 
 
 open class BaseActivityImpl : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        MobileAds.initialize(this, "ca-app-pub-6161644866653093~1103756003")
-    }
+    val adView : AdView? by lazy { findViewById<AdView?>(com.lazard.nyapp.nyapp.R.id.adView) }
 
     override fun onPostCreate(savedInstanceState: Bundle?){
         super.onPostCreate(savedInstanceState)
-        val adView = AdView(this)
-        adView.adSize = AdSize.BANNER
-        adView.adUnitId = "ca-app-pub-6161644866653093/1008510949"
+        adView?:return
+
+        MobileAds.initialize(this, resources.getString(com.lazard.nyapp.nyapp.R.string.admob_applicationId))
+
+//        val android_id = Settings.Secure.getString(
+//            getContentResolver(),
+//            Settings.Secure.ANDROID_ID
+//        )
+//        Log.e("android_id",""+android_id)
+
+        val adRequest = AdRequest.Builder()
+            .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+            .addTestDevice("E9E948CD0C00C42A7339DD77DCF5A333")//samsung test device
+            .build()
+        adView?.loadAd(adRequest)
     }
 
 
